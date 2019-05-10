475eef1d7287382416e2be11b6f656606d006b2d
samuelomondi22 <omondisam696@gmail.com>
2019-05-10 16:46:52 +0300

    static-twitter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Tweet =() => {
+    return(
+        <div className='tweet'>
+            <Avatar/>
+            <div className='content'>
+                <NameWithHandle/>
+                <Time/>
+                <Message/>
+                <div className='buttons'>
+                <ReplyButton/>
+                <RetweetButton/>
+                <LikeButton/>
+                <MoreOptionsButton/>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Avatar =() => {
+    return(
+        <img src='https://www.gravatar.com/avatar/03502ca69f5525bbf91142cb89059eff' className='avatar' alt='avatar'/>
+    );
+}
+
+const Message =() => {
+    return(
+        <div className='message'>
+        I am starting to love how this React is treating me...
+        </div>
+    );
+}
+
+const NameWithHandle =() => {
+    return(
+        <span className='name-with-handle'>
+        <span className='name'>
+        Samuel Omondi
+        </span>
+        <span className='handle'>
+            @omondisam696
+        </span>
+        </span>
+    )
+}
+//when writting arrow function with one expression it can be written without braces
+const Time = () => (
+    <span className='time'>
+        3h
+    </span>
+)
+const ReplyButton = () => (
+    <i className='fa fa-reply reply-button'></i>
+)
+const RetweetButton = () => (
+    <i className='fa fa-retweet retweet-button'></i>
+)
+const LikeButton = () => (
+    <i className='fa fa-heart like-button'></i>
+)
+const MoreOptionsButton = () => (
+    <i className='fa fa-ellipsis-h more-options-button'></i>
+)
+
+ReactDOM.render(
+    <Tweet/>,
+    document.getElementById('root'),
+)
